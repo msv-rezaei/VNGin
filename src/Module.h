3c0e329c763826aaf57c870fcd1b8c4a9b19fc65
MSV <mahdirezaei.vincent@gmail.com>
2023-09-02 13:49:47 +0330

    Add Module.h

---
+++ src/Module.h
@@ -0,0 +1,18 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+namespace VNGin {
+    class Module {
+        class Entity;
+    public: 
+        virtual const char* GetType(void) = 0;
+    protected: 
+        Module(Entity* owner = nullptr) : owner{owner} {} 
+        virtual void Start(void) = 0;
+        virtual void Update(void) = 0;
+
+        Entity* owner;
+    };
+}
+
+#endif
