82aeabee50666325fd3f7c6f724946c0d55217eb
Mahdi Rezaei <mahdirezaei.vincent@gmail.com>
2023-10-08 19:21:01 +0330

    Update InputSystem

--- src/InputSystem.cpp
+++ src/InputSystem.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 
 std::map<SDL_Scancode, bool> VNGin::InputSystem::keyMap;
+std::map<SDL_Scancode, std::vector<void(*)(bool)>> VNGin::InputSystem::callbackMap;
 
 int VNGin::InputSystem::GetKey(SDL_Scancode code) {
     auto iter = keyMap.find(code);
@@ -12,6 +13,25 @@ int VNGin::InputSystem::GetKey(SDL_Scancode code) {
     }
 
     return iter->second;
+}
+
+void VNGin::InputSystem::AddCallback(SDL_Scancode code, void(*callback)(bool)) {
+    auto iter = callbackMap.find(code); 
+    if(iter != callbackMap.end()) {
+        iter->second.push_back(callback);
+        return; 
+    }
+
+    callbackMap.insert({code, {callback}});
+}
+
+void VNGin::InputSystem::RemoveCallback(SDL_Scancode code, void(*callback)(bool)) {
+    auto iter = callbackMap.find(code); 
+    if(iter != callbackMap.end()) {
+        auto vecIter = std::find(iter->second.begin(), iter->second.end(), callback);
+        if(vecIter != iter->second.end()) 
+            iter->second.erase(vecIter);
+    }
 }
 
 void VNGin::InputSystem::UpdateMap(SDL_KeyboardEvent* event, bool value) {
@@ -32,9 +52,11 @@ void VNGin::InputSystem::Update() {
             exit(0); 
             break;
         case SDL_KEYDOWN: 
+            for(auto callback : callbackMap[event.key.keysym.scancode]) callback(true);
             UpdateMap(&event.key, true); 
             break; 
         case SDL_KEYUP: 
+            for(auto callback : callbackMap[event.key.keysym.scancode]) callback(false);
             UpdateMap(&event.key, false); 
             break; 
         default: 
--- src/InputSystem.h
+++ src/InputSystem.h
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL.h>
 
+#include <vector> 
 #include <map>
 
 namespace VNGin {
@@ -11,10 +12,13 @@ namespace VNGin {
         friend class Game; 
     public: 
         static int GetKey(SDL_Scancode code);
+        static void AddCallback(SDL_Scancode code, void(*callback)(bool)); 
+        static void RemoveCallback(SDL_Scancode code, void(*callback)(bool));
     private: 
         static void Update(void);
         static void UpdateMap(SDL_KeyboardEvent* event, bool value);
         static std::map<SDL_Scancode, bool> keyMap;
+        static std::map<SDL_Scancode, std::vector<void(*)(bool)>> callbackMap;
     };
 }
 
