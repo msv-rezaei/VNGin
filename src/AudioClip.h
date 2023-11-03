7db203090f2a3706a9ee71bea97d9a897afc72ad
Mahdi Rezaei <mahdirezaei.vincent@gmail.com>
2023-11-03 09:34:28 +0330

    Update AudioClip and Jukebox memory management

--- src/AudioClip.h
+++ src/AudioClip.h
@@ -8,6 +8,7 @@ namespace VNGin {
     class AudioClip {
     public: 
         AudioClip(const char* path) : clip{Game::LoadSFX(path)} {} 
+        ~AudioClip() { Mix_FreeChunk(clip); }
         void Play() { Mix_PlayChannel(-1, clip, 0); }
     private: 
         Mix_Chunk* clip;
--- src/Jukebox.cpp
+++ src/Jukebox.cpp
@@ -4,12 +4,15 @@
 Mix_Music* VNGin::Jukebox::music;
 
 void VNGin::Jukebox::Set(const char* path) {
-    Jukebox::music = Game::LoadMusic(path);
+    if(music != NULL) 
+        Mix_FreeMusic(music);
+
+    music = Game::LoadMusic(path);
 }
 
 void VNGin::Jukebox::Play() {
     if(Mix_PlayingMusic() == 0) {
-        Mix_PlayMusic(Jukebox::music, -1);
+        Mix_PlayMusic(music, -1);
     }
     else if(Mix_PausedMusic() == 1) {
             Mix_ResumeMusic();
