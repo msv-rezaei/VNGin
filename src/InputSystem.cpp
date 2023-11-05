6ea05358724ae3a751c10789eb2dd21aead61b99
Mahdi Rezaei <mahdirezaei.vincent@gmail.com>
2023-11-05 19:17:46 +0330

    Implement mouse buttons input actions

--- src/InputSystem.cpp
+++ src/InputSystem.cpp
@@ -5,10 +5,19 @@
 #include <algorithm>
 
 std::map<SDL_Scancode, VNGin::KeyState> VNGin::InputSystem::keyMap;
+std::vector<VNGin::KeyState> VNGin::InputSystem::mouseButtons = std::vector<VNGin::KeyState>(3);
 
 bool VNGin::InputSystem::GetKey(SDL_Scancode code) {
     auto iter = FindOrAddKey(code);
     return iter->second.currentState;
+}
+
+bool VNGin::InputSystem::GetMouseButton(int index) {
+    if(index < 0 || index > 2) 
+        return false; 
+    
+    //
+    return mouseButtons[index].currentState;
 }
 
 bool VNGin::InputSystem::GetKeyDown(SDL_Scancode code) {
@@ -16,9 +25,23 @@ bool VNGin::InputSystem::GetKeyDown(SDL_Scancode code) {
     return iter->second.lastState == false && iter->second.currentState == true;
 }
 
+bool VNGin::InputSystem::GetMouseButtonDown(int index) {
+    if(index < 0 || index > 2) 
+        return false;
+    
+    return mouseButtons[index].lastState == false && mouseButtons[index].currentState == true;
+}
+
 bool VNGin::InputSystem::GetKeyUp(SDL_Scancode code) {
     auto iter = FindOrAddKey(code);
     return iter->second.lastState == true && iter->second.currentState == false;
+}
+
+bool VNGin::InputSystem::GetMouseButtonUp(int index) {
+    if(index < 0 || index > 2) 
+        return false;
+    
+    return mouseButtons[index].lastState == true && mouseButtons[index].currentState == false;
 }
 
 VNGin::Vector VNGin::InputSystem::GetMousePosition() {
@@ -45,12 +68,20 @@ void VNGin::InputSystem::UpdateMap(SDL_Scancode code, bool value) {
     catch(std::out_of_range e) { return; }
 }
 
+void VNGin::InputSystem::UpdateMouseState(int buttonIndex, bool value) {
+    mouseButtons[buttonIndex-1].lastState = mouseButtons[buttonIndex-1].currentState; 
+    mouseButtons[buttonIndex-1].currentState = value;
+}
+
 void VNGin::InputSystem::Update() {
     SDL_Event event; 
 
-    for(auto &key : keyMap) {
+    for(auto &key : keyMap) 
         key.second.lastState = key.second.currentState;
-    }
+    
+    for(auto &button : mouseButtons) 
+        button.lastState = button.currentState;
+
 
     while(SDL_PollEvent(&event)) {
         switch(event.type) {
@@ -63,6 +94,12 @@ void VNGin::InputSystem::Update() {
             break; 
         case SDL_KEYUP: 
             UpdateMap(event.key.keysym.scancode, false); 
+            break; 
+        case SDL_MOUSEBUTTONDOWN: 
+            UpdateMouseState(event.button.button, true);
+            break; 
+        case SDL_MOUSEBUTTONUP: 
+            UpdateMouseState(event.button.button, false);
             break; 
         default: 
             break;
--- src/InputSystem.h
+++ src/InputSystem.h
@@ -21,15 +21,20 @@ namespace VNGin {
         friend class Game; 
     public: 
         static bool GetKey(SDL_Scancode code);
+        static bool GetMouseButton(int index);
         static bool GetKeyDown(SDL_Scancode code);
+        static bool GetMouseButtonDown(int index);
         static bool GetKeyUp(SDL_Scancode code);
+        static bool GetMouseButtonUp(int index);
         static Vector GetMousePosition(void);
     private: 
         static std::map<SDL_Scancode, KeyState>::iterator FindOrAddKey(SDL_Scancode code);
 
         static void Update(void);
         static void UpdateMap(SDL_Scancode event, bool value);
+        static void UpdateMouseState(int buttonIndex, bool value);
         static std::map<SDL_Scancode, KeyState> keyMap;
+        static std::vector<KeyState> mouseButtons;
     };
 }
 
